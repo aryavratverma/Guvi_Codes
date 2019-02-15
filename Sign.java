1a28368c5b2db178268480f6980f0d431a5a384d
Aryavrat <aryavrat97@gmail.com>
2019-02-14 22:51:47 -0800

    Create Sign.java

---
+++ Sign.java
@@ -0,0 +1,24 @@
+
+
+import java.io.*;
+
+import java.util.Scanner;
+
+
+class Sign{
+
+public static void main (String[] args) {
+
+Scanner sc = new Scanner(System.in); 
+
+int n =sc.nextInt();
+
+if(n>=0){
+
+System.out.println("POSITIVE");
+
+else{
+
+System.out.println("NEGATIVE");
+}}
+}
