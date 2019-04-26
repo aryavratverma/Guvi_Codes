bd5f2e3a83ed79292a4c8e893aa4905fe11fe8a0
aryavratverma <aryavrat97@gmail.com>
2019-04-25 22:28:12 -0700

    Create vovel.py

---
+++ vovel.py
@@ -0,0 +1,10 @@
+l=["a","e","i","o","u","A","E","I","O","U"]
+l1=["b","c","d","f","g","h","i","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
+a=input()
+
+if a in l:
+    print("Vovel")
+elif a in l1:
+    print("Consonant")
+else:
+    print("Invalid")
