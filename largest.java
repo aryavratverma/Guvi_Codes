c93b82e33111813e2917dbfe14d8f4ebc2085c9b
aryavratverma <aryavrat97@gmail.com>
2019-05-17 22:26:32 -0700

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+ 
+public class GFG {
+
+	public static void main (String[] args) throws IOException {
+	
+    BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+
+	    String[] str = reader.readLine().split(" ");
+	 
+   int a= Integer.parseInt(str[0]);
+	 
+   int b=Integer.parseInt(str[1]);
+	
+    int c=Integer.parseInt(str[2]);
+	
+    if(a>b && a>c){
+	        
+System.out.println(a);
+	    
+}else if(b>a && b>c){
+	
+        System.out.println(b);
+	 
+   }else{
+	       
+ System.out.println(c);
+	  
+  }
+	}}
