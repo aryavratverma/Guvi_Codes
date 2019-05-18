4cc8886a1a35c4e7eb49ebd254ad4a68c66e7645
aryavratverma <aryavrat97@gmail.com>
2019-05-17 22:49:54 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+class GFG {
+	
+public static void main (String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+int n= sc.nextInt();
+		
+if(n>0){
+		
+for(int i=0;i<n;i++){
+	
+	    System.out.println("Hello");
+
+		}}
+	}
+}
